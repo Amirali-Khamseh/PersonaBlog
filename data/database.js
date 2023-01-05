7eb631af133842388c474f70064d7cabe9fb458d
Amir <72108341+Amirali-Khamseh@users.noreply.github.com>
2023-01-05 14:02:39 +0100

    database file modfied with the general Database server infromation

--- data/database.js
+++ data/database.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
-    host:'localhost',
-    database:'blog',
-    user:'root',
-    password:'01amirali'
+    host:'...',
+    database:'...',
+    user:'...',
+    password:'...'
 });
 
 module.exports = pool;
